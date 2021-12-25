75fda7656e4873a4eecff6c0ae92b6c330d1aaf5
SnowpawStudios <49541454+SnowpawStudios@users.noreply.github.com>
2021-12-25 12:39:20 +1100

    first commit

---
+++ passgen/generator/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('uppercase'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('special'):
+        characters.extend('!@#$%^&*()_+')
+    if request.GET.get('numbers'):
+        characters.extend('1234567890')
+    length = request.GET.get('length', 12)
+    
+    
+    thepassword = ''
+    for x in range(int(length)):
+        thepassword += random.choice(characters)
+
+    context = {
+        'password': thepassword,
+    }
+    return render(request, 'generator/password.html', context)
